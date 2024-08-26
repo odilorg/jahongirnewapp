dfde57f9db11bea7a058211effa21c923fcf85c4
Odiljon <odilorg@gmail.com>
2024-08-26 15:30:02 +0500

    start date filter

--- app/Filament/Resources/TerminalCheckResource.php
+++ app/Filament/Resources/TerminalCheckResource.php
@@ -2,16 +2,18 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\TerminalCheckResource\Pages;
-use App\Filament\Resources\TerminalCheckResource\RelationManagers;
-use App\Models\TerminalCheck;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Support\RawJs;
+use App\Models\TerminalCheck;
+use Filament\Resources\Resource;
+use Filament\Tables\Enums\FiltersLayout;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TerminalCheckResource\Pages;
+use App\Filament\Resources\TerminalCheckResource\RelationManagers;
 
 class TerminalCheckResource extends Resource
 {
@@ -23,9 +25,12 @@ class TerminalCheckResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('amount')
-                ->numeric()
-                ->prefix('UZS'),
+            Forms\Components\TextInput::make('amount')
+            ->prefix('UZS')
+            ->mask(RawJs::make('$money($input)'))
+            ->stripCharacters(',')
+            ->numeric(),
+           
                 
             Forms\Components\DatePicker::make('check_date')
                 ->required()
@@ -33,11 +38,12 @@ class TerminalCheckResource extends Resource
                 ->maxDate(now()),
             Forms\Components\Select::make('card_type')
                 ->options([
-                    'humo_ok' => 'Humo OK',
-                    'humo_ytt' => 'Humo YTT',
-                    'uzcard_ok' => 'Uzcard OK',
-                    'uzcard_ytt' => 'Uzcard YTT',
+                    'Humo OK' => 'Humo OK',
+                    'Humo YTT' => 'Humo YTT',
+                    'Uzcard OK' => 'Uzcard OK',
+                    'Uzcard YTT' => 'Uzcard YTT',
                 ])
+                
             ]);
     }
 
@@ -45,11 +51,28 @@ class TerminalCheckResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('check_date')
+                ->sortable(),
+                Tables\Columns\TextColumn::make('card_type')
+                ->badge()
+                ->color(fn (string $state): string => match ($state) {
+                    'Humo OK' => 'info',
+                    'Humo YTT' => 'warning',
+                    'Uzcard OK' => 'success',
+                    'Uzcard YTT' => 'danger',
+                }),
+                Tables\Columns\TextColumn::make('amount'),
+
             ])
             ->filters([
-                //
-            ])
+                Tables\Filters\SelectFilter::make('card_type')
+                ->options([
+                    'Humo OK' => 'Humo OK',
+                    'Humo YTT' => 'Humo YTT',
+                    'Uzcard OK' => 'Uzcard OK',
+                    'Uzcard YTT' => 'Uzcard YTT',
+                ])
+                ], layout: FiltersLayout::AboveContent) 
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
