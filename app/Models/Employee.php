05929dec0fc972952d4693c0f13b40921fcb450d
Odiljon <odilorg@gmail.com>
2024-09-08 13:14:34 +0500

    employee

---
+++ app/Models/Employee.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'lang_spoken' => 'array',
+    ];
+
+    protected $fillable = [
+        'first_name', 'last_name', 'email', 'phone01', 'phone02', 'lang_spoken', 'guide_image',
+        'extra_info',
+        'school',
+        'passport_image',
+        'fathers_name'
+    ];
+
+    public function languages(): BelongsToMany
+    {
+        return $this->belongsToMany(SpokenLanguage::class, 'language_employee');
+    }
+}
