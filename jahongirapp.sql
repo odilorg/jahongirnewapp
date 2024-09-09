bc786342ac7f378089c0235a10d4aca76d5f7a6d
Odiljon <odilorg@gmail.com>
2024-09-09 18:26:31 +0500

    merged

--- jahongirapp.sql
+++ jahongirapp.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
+--rr
 -- Host: 127.0.0.1
 -- Generation Time: Sep 07, 2024 at 09:29 AM
 -- Server version: 10.4.28-MariaDB
