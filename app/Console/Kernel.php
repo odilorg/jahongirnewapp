8e6a9f7aaf33bd07706ae59e23067e70b896034d
odil <odilorg@gmail.com>
2024-09-12 21:47:24 +0500

    56

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
         foreach ($scheduledMessages as $message) {
             $runAt = Carbon::parse($message->scheduled_at);
 
-          //  $frequencyMethod = $this->mapFrequencyToMethod($message->frequency);
+            $frequencyMethod = $this->mapFrequencyToMethod($message->frequency);
 
             // Get the related chat from the Chat model
             $chat = $message->chat->chat_id;
@@ -35,8 +35,8 @@ class Kernel extends ConsoleKernel
                 ->at($runAt->format('H:i'))
                 ->when(function () use ($message) {
                     return now()->isSameDay($message->scheduled_at);
-                });
-             //   ->{$frequencyMethod}($runAt->day, $runAt->format('H:i'));
+                })
+                ->{$frequencyMethod}($runAt->day, $runAt->format('H:i'));
             }
         }
     }
