959680ef3986c0bb8c257078cfcce2a509165bb6
odil <odilorg@gmail.com>
2024-09-12 22:10:05 +0500

    fg

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,11 +33,12 @@ class Kernel extends ConsoleKernel
                 })
                 ->timezone('Asia/Samarkand')
                 //->at($runAt->format('H:i'))
-                ->dailyAt($runAt->format('H:i')) // Schedule the job daily at the specified time
+               // ->dailyAt($runAt->format('H:i')) // Schedule the job daily at the specified time
+                 ->{$frequencyMethod}($runAt->format('H:i')) // Use the initialized $frequencyMethod
                 ->when(function () use ($message) {
                     return now()->isSameDay($message->scheduled_at);
                 });
-              //  ->{$frequencyMethod}($runAt->day, $runAt->format('H:i')); // Use the initialized $frequencyMethod
+              
             }
         }
     }
