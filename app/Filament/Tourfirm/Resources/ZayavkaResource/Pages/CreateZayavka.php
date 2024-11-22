0757738c5252a061d01794cc9a695184e3bedc62
odil <odilorg@gmail.com>
2024-11-22 22:52:09 +0500

    tour panel

---
+++ app/Filament/Tourfirm/Resources/ZayavkaResource/Pages/CreateZayavka.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Tourfirm\Resources\ZayavkaResource\Pages;
+
+use App\Filament\Tourfirm\Resources\ZayavkaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateZayavka extends CreateRecord
+{
+    protected static string $resource = ZayavkaResource::class;
+}
