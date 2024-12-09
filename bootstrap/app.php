33c04e8a6bf2c6f3de9ea52f885c91121bd44d60
odil <odilorg@gmail.com>
2024-12-09 23:29:57 +0500

    fd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
