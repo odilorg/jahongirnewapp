0239d77f726ba9796be2254594a63888754df9c6
Odiljon <odilorg@gmail.com>
2024-12-11 09:24:42 +0500

    fixe dbug

--- resources/views/contracts/contract.blade.php
+++ resources/views/contracts/contract.blade.php
@@ -157,13 +157,13 @@
             <table style="width: 100%; border-collapse: collapse;">
                 <tr>
                     <td style="width: 50%; vertical-align: top; padding: 10px; border-right: 1px solid black;">
-                        <strong>{{ $contract->$hotel->name }} {{ $contract->$hotel->official_name }}</strong><br>
-                        {{ $contract->$hotel->account_number }}<br>
-                        {{ $contract->$hotel->bank_name }} МФО {{ $contract->$hotel->bank_mfo }}<br>
-                        ИНН {{ $contract->$hotel->inn }} <br>
-                        {{ $contract->$hotel->address }}<br>
-                        Тел: {{ $contract->$hotel->phone }}<br>
-                        Email: {{ $contract->$hotel->email }}<br>
+                        <strong>{{ $contract->hotel->name }} при {{ $contract->hotel->official_name }}</strong><br>
+                        р/с {{ $contract->hotel->account_number }}<br>
+                        {{ $contract->hotel->bank_name }} МФО {{ $contract->hotel->bank_mfo }}<br>
+                        ИНН {{ $contract->hotel->inn }} <br>
+                        {{ $contract->hotel->address }}<br>
+                        Тел: {{ $contract->hotel->phone }}<br>
+                        Email: {{ $contract->hotel->email }}<br>
                         Директор: {{ $contract->hotel->director_name }}<br>
                     </td>
                     <td style="width: 50%; vertical-align: top; padding: 10px;">
