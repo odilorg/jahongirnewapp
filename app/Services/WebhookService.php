f5eae8182d53a7397bf0793abf8007b440d4cee0
odil <odilorg@gmail.com>
2025-01-24 19:36:41 +0500

    web url

--- app/Services/WebhookService.php
+++ app/Services/WebhookService.php
@@ -12,7 +12,7 @@ class WebhookService
     public function __construct()
     {
         // Set your webhook URL here (you can also use .env for this)
-        $this->webhookUrl = env('WEBHOOK_URL', 'https://jahongir-app.uz/webhook/ai-instructions');
+        $this->webhookUrl = env('WEBHOOK_URL', 'https://jahongir-app.uz/n8n/webhook-test/35f85b50-628f-4bdc-b0dd-6cf57ed392a0');
     }
 
     public function sendWebhook($data)
