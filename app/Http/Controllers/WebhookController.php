44b9cc44e3d2a0f663470b1443098ae86668eba5
Odil <odilorg@gmail.com>
2025-05-09 18:20:21 +0500

    token added

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,14 +10,19 @@ class WebhookController extends Controller
 {
     public function handleTourBooking(Request $request)
     {
+        // 🔒 API key protection
+        if ($request->header('X-API-KEY') !== env('N8N_SECRET_KEY')) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+    
         // Get data
         $data = $request->all();
-
+    
         // Split full name
         $nameParts = explode(' ', $data['Name'], 2);
         $firstName = $nameParts[0];
         $lastName = $nameParts[1] ?? '';
-
+    
         DB::beginTransaction();
         try {
             // Insert into guests table
@@ -28,25 +33,24 @@ class WebhookController extends Controller
                 'phone' => $data['Phone'],
                 'country' => $data['Country'],
                 'number_of_people' => $data['Number of people'],
-
                 'full_name' => $data['Name'], // optional if you need to store full
             ]);
-
+    
             // Insert into bookings table
             $booking = Booking::create([
                 'guest_id' => $guest->id,
-               
                 'booking_start_date_time' => $data['Departure Date Time'],
                 'pickup_location' => $data['Meeting Point'],
                 'special_requests' => $data['Mesage'] ?? null,
             ]);
-
+    
             DB::commit();
             return response()->json(['status' => 'success'], 200);
-
+    
         } catch (\Exception $e) {
             DB::rollBack();
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+    
 }
