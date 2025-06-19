8078555556830f3dfe3f11af686615fac7891961
odil <odilorg@gmail.com>
2025-06-19 15:47:02 +0500

    ttl 500

--- app/Services/OctoPaymentService.php
+++ app/Services/OctoPaymentService.php
@@ -99,7 +99,7 @@ class OctoPaymentService
             'return_url'  => url('/payment/success'),
             'notify_url'  => route('octo.callback'),
             'language'    => 'en',
-            'ttl'         => 15,
+            'ttl'         => 500,
         ];
 
         if (!$payload['tsp_id']) {
