dde332bfb02020d82fd04aec9833ee33936085bc
odilorg <odilorg@gmail.com>
2025-09-23 12:01:04 +0500

    no widgets

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,9 +41,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
-                \App\Filament\Widgets\CurrentShiftWidget::class,
                 \App\Filament\Widgets\DrawerBalanceWidget::class,
-                \App\Filament\Widgets\MultiCurrencyBalanceWidget::class,
                 \App\Filament\Widgets\CompactLanguageSwitcher::class,
             ])
             ->middleware([
